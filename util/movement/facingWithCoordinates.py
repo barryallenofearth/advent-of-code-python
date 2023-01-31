8bef432b1eaebf0687a09223b5013de7f5d117c7
Lisa_und_Michi <lordrevan1999@web.de>
2023-01-31 21:20:00 +0100

    prepare util classes

---
+++ util/movement/facingWithCoordinates.py
@@ -0,0 +1,56 @@
+from util.movement.coordinates import Coordinates
+
+UP = "^"
+RIGHT = ">"
+DOWN = "v"
+LEFT = "<"
+
+ROTATE_RIGHT = "R"
+ROTATE_LEFT = "L"
+
+'''The coordinate system is assumed to be from top y=0 to increasing values while moving down. 
+Left and Right are handled as expected'''
+
+
+def move_forward(position: Coordinates, facing: str, steps: int):
+    if facing == UP:
+        position.y = position.y - steps
+    elif facing == RIGHT:
+        position.x = position.x + steps
+    elif facing == DOWN:
+        position.y = position.y + steps
+    elif facing == LEFT:
+        position.x = position.x - steps
+    else:
+        raise ValueError(f"'{facing}' is not a valid facing")
+
+
+def rotate(current_facing: str, rotation: str):
+    if rotation != ROTATE_LEFT and rotation != ROTATE_RIGHT:
+        raise ValueError(f"'{rotation}' is not a valid rotation")
+
+    if current_facing == UP:
+        if rotation == ROTATE_RIGHT:
+            return RIGHT
+        elif rotation == ROTATE_LEFT:
+            return LEFT
+
+    elif current_facing == RIGHT:
+        if rotation == ROTATE_RIGHT:
+            return DOWN
+        elif rotation == ROTATE_LEFT:
+            return UP
+
+    elif current_facing == DOWN:
+        if rotation == ROTATE_RIGHT:
+            return RIGHT
+        elif rotation == ROTATE_LEFT:
+            return LEFT
+
+    elif current_facing == LEFT:
+        if rotation == ROTATE_RIGHT:
+            return UP
+        elif rotation == ROTATE_LEFT:
+            return DOWN
+    else:
+        raise ValueError(f"'{current_facing}' is not a valid facing")
