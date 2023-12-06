5b264eee9b09478c822eb9b5dacc64de392320a4
Michael <lordrevan1999@web.de>
2023-12-06 21:51:38 +0100

    AoC-2023 day 5 part 2 complete

--- aoc_2023/day5/food_almanac_part2.py
+++ aoc_2023/day5/food_almanac_part2.py
@@ -19,27 +19,75 @@ class Mapping:
         self.target_start_id = target_start_id
         self.target_stop_id = target_start_id + range - 1
 
-    def get_source_id(self, target_id: int) -> int:
+    def __get_source_id(self, target_id: int) -> int:
         if self.target_start_id <= target_id <= self.target_stop_id:
             return self.source_start_id + (target_id - self.target_start_id)
 
         return -1
 
+    def get_source_ids(self, target_id_range: tuple[int:int]) -> tuple[set[tuple[int::int]]:set[tuple[int::int]]]:
+        mapped_ids = set()
+        unmapped_ids = set()
+        if target_id_range[1] < self.target_start_id or target_id_range[0] > self.target_stop_id:
+            # completely outside of mapping range
+            unmapped_ids.add(target_id_range)
+        elif target_id_range[0] >= self.target_start_id and target_id_range[1] <= self.target_stop_id:
+            # completely within range
+            mapped_start_id = self.__get_source_id(target_id_range[0])
+            mapped_stop_id = self.__get_source_id(target_id_range[1])
+            mapped_ids.add((mapped_start_id, mapped_stop_id))
+        elif target_id_range[0] < self.target_start_id and target_id_range[1] <= self.target_stop_id:
+            # partially smaller, end within range
+
+            mapped_start_id = target_id_range[0]
+            mapped_stop_id = self.target_start_id - 1
+            unmapped_ids.add((mapped_start_id, mapped_stop_id))
+
+            mapped_start_id = self.__get_source_id(self.target_start_id)
+            mapped_stop_id = self.__get_source_id(target_id_range[1])
+            mapped_ids.add((mapped_start_id, mapped_stop_id))
+        elif target_id_range[0] >= self.target_start_id and target_id_range[1] > self.target_stop_id:
+            # partially larger, beginning within range
+            mapped_start_id = self.__get_source_id(target_id_range[0])
+            mapped_stop_id = self.__get_source_id(self.target_stop_id)
+            mapped_ids.add((mapped_start_id, mapped_stop_id))
+
+            mapped_start_id = self.target_stop_id + 1
+            mapped_stop_id = target_id_range[1]
+            unmapped_ids.add((mapped_start_id, mapped_stop_id))
+        elif target_id_range[0] >= self.target_start_id and target_id_range[1] > self.target_stop_id:
+            # beginning smaller, end larger than range
+            mapped_start_id = target_id_range[0]
+            mapped_stop_id = self.target_start_id - 1
+            unmapped_ids.add((mapped_start_id, mapped_stop_id))
+
+            mapped_start_id = self.__get_source_id(self.target_start_id)
+            mapped_stop_id = self.__get_source_id(self.target_stop_id)
+            mapped_ids.add((mapped_start_id, mapped_stop_id))
+
+            mapped_start_id = self.target_stop_id + 1
+            mapped_stop_id = target_id_range[1]
+            unmapped_ids.add((mapped_start_id, mapped_stop_id))
+
+        return mapped_ids, unmapped_ids
+
     def __repr__(self):
         return f"{self.name}: source_start_id={self.source_start_id}, target_range={self.target_start_id} to {self.target_stop_id}"
 
 
-def map_id(target_id: int, mappings: list[Mapping]):
-    current_id = -1
-    for mapping in mappings:
-        mapped_id = mapping.get_source_id(target_id)
-        if mapped_id > 0:
-            current_id = mapped_id
-            break
-    if current_id < 0:
-        current_id = target_id
+def map_id(target_id_list: set[tuple[int:int]], mappings: list[Mapping]) -> set[tuple[int:int]]:
+    all_mapped_source_ids = set()
 
-    return current_id
+    all_unmapped_target_ids = set(target_id_list)
+    for mapping in mappings:
+        for _ in range(len(all_unmapped_target_ids)):
+            current_range = all_unmapped_target_ids.pop()
+            mapped_tuple_set, unmapped_tuple_set = mapping.get_source_ids(current_range)
+            all_mapped_source_ids.update(mapped_tuple_set)
+            all_unmapped_target_ids.update(unmapped_tuple_set)
+
+    all_mapped_source_ids.update(all_unmapped_target_ids)
+    return all_mapped_source_ids
 
 
 def display_mappings(name: str, mappings: list[Mapping]):
@@ -48,7 +96,7 @@ def display_mappings(name: str, mappings: list[Mapping]):
         print(mapping)
 
 
-lines = riddle_reader.read_file(riddle_reader.TEST_RIDDLE_FILE)
+lines = riddle_reader.read_file(riddle_reader.RIDDLE_FILE)
 
 SEEDS_TO_SOIL = []
 SOIL_TO_FERTILIZER = []
@@ -60,69 +108,51 @@ HUMIDITY_TO_LOCATION = []
 
 currentMapList = SEEDS_TO_SOIL
 currentName = ""
-seeds = []
+seeds = set()
 for line in lines:
     if line.startswith("seeds:"):
         seed_strings = re.sub(r"seeds:\s+", "", lines[0].strip()).split(" ")
         for index in range(0, len(seed_strings), 2):
-            startingNumber = int(seed_strings[index])
-            for seed in range(int(seed_strings[index + 1])):
-                seeds.append(startingNumber + seed)
+            starting_number = int(seed_strings[index])
+            seed_id_range = int(seed_strings[index + 1])
+            seeds.add((starting_number, starting_number + seed_id_range))
         print(f"seeds: {seeds}")
     elif line == SEEDS_TO_SOIL_MAP_START:
         currentMapList = SEEDS_TO_SOIL
         currentName = SEEDS_TO_SOIL_MAP_START
-        display_mappings("seeds to soil:", SEEDS_TO_SOIL)
     elif line == SOIL_TO_FERTILIZER_MAP_START:
         currentMapList = SOIL_TO_FERTILIZER
         currentName = SOIL_TO_FERTILIZER_MAP_START
-        display_mappings("soil to fertilizer:", SOIL_TO_FERTILIZER)
     elif line == FERTILIZER_TO_WATER_MAP_START:
         currentMapList = FERTILIZER_TO_WATER
         currentName = FERTILIZER_TO_WATER_MAP_START
-        display_mappings("fertilizer to water:", FERTILIZER_TO_WATER)
     elif line == WATER_TO_LIGHT_MAP_START:
         currentMapList = WATER_TO_LIGHT
         currentName = WATER_TO_LIGHT_MAP_START
-        display_mappings("water to light:", WATER_TO_LIGHT)
     elif line == LIGHT_TO_TEMPERATURE_MAP_START:
         currentMapList = LIGHT_TO_TEMPERATURE
         currentName = LIGHT_TO_TEMPERATURE_MAP_START
-        display_mappings("light to temperature:", LIGHT_TO_TEMPERATURE)
     elif line == TEMPERATURE_TO_HUMIDITY_MAP_START:
         currentMapList = TEMPERATURE_TO_HUMIDITY
         currentName = TEMPERATURE_TO_HUMIDITY_MAP_START
-        display_mappings("temperature to humidity:", TEMPERATURE_TO_HUMIDITY)
     elif line == HUMIDITY_TO_LOCATION_MAP_START:
         currentMapList = HUMIDITY_TO_LOCATION
         currentName = HUMIDITY_TO_LOCATION_MAP_START
-        display_mappings("humidity to location:", HUMIDITY_TO_LOCATION)
     elif len(line) != 0:
         numbers = line.split(" ")
         currentMapList.append(Mapping(currentName, int(numbers[0]), int(numbers[1]), int(numbers[2])))
 
+mimimum_location = 100000000000000
+id_ranges = map_id(seeds, SEEDS_TO_SOIL)
+id_ranges = map_id(id_ranges, SOIL_TO_FERTILIZER)
+id_ranges = map_id(id_ranges, FERTILIZER_TO_WATER)
+id_ranges = map_id(id_ranges, WATER_TO_LIGHT)
+id_ranges = map_id(id_ranges, LIGHT_TO_TEMPERATURE)
+id_ranges = map_id(id_ranges, TEMPERATURE_TO_HUMIDITY)
+id_ranges = map_id(id_ranges, HUMIDITY_TO_LOCATION)
+print(id_ranges)
+for id_range in id_ranges:
+    if id_range[0] < mimimum_location:
+        mimimum_location = id_range[0]
 
-
-
-
-
-
-
-
-
-mapped_seeds_to_location = []
-for seed in seeds:
-    current_id = map_id(seed, SEEDS_TO_SOIL)
-    current_id = map_id(current_id, SOIL_TO_FERTILIZER)
-    current_id = map_id(current_id, FERTILIZER_TO_WATER)
-    current_id = map_id(current_id, WATER_TO_LIGHT)
-    current_id = map_id(current_id, LIGHT_TO_TEMPERATURE)
-    current_id = map_id(current_id, TEMPERATURE_TO_HUMIDITY)
-    current_id = map_id(current_id, HUMIDITY_TO_LOCATION)
-    mapped_seeds_to_location.append(current_id)
-
-
-print(f"The mapped location ids are: {mapped_seeds_to_location}")
-mapped_seeds_to_location.sort()
-
-print(f"The lowest location id is {mapped_seeds_to_location[0]}")
+print(f"The lowest location id is {mimimum_location}")
