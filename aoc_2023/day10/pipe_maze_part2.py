3688e666fc9a2f332085793440416ac3bff08603
Michael <lordrevan1999@web.de>
2023-12-11 00:48:09 +0100

    AoC-2023 day 10 part 2 all examples are working

--- aoc_2023/day10/pipe_maze_part2.py
+++ aoc_2023/day10/pipe_maze_part2.py
@@ -34,7 +34,7 @@ def get_symbol_neighbours(coordinates_with_symbols: dict[Coordinates, str], conn
         if coordinates2 not in connected_coordinates:
             if are_pipes_connected(coordinates1, coordinates2, coordinates_with_symbols):
                 connected_coordinates.append(coordinates2)
-                coordinates_with_facing[coordinates2] = next_facing
+                coordinates_with_facing[coordinates1] = next_facing
 
     current_coordinates = connected_coordinates[-1]
     current_symbol = coordinates_with_symbols[current_coordinates]
@@ -46,12 +46,13 @@ def get_symbol_neighbours(coordinates_with_symbols: dict[Coordinates, str], conn
 
     if current_symbol == "S":
         current_facing = update_starting_symbol(coordinates_with_symbols, current_coordinates, north, east, south, west)
+        current_symbol = coordinates_with_symbols[current_coordinates]
         coordinates_with_facing[current_coordinates] = current_facing
 
-    add_if_not_yet_present(current_coordinates, north, facing.UP)
-    add_if_not_yet_present(current_coordinates, east, facing.RIGHT)
-    add_if_not_yet_present(current_coordinates, south, facing.DOWN)
-    add_if_not_yet_present(current_coordinates, west, facing.LEFT)
+    add_if_not_yet_present(current_coordinates, north, facing.UP if current_symbol == "|" or north not in coordinates_with_symbols else coordinates_with_symbols[current_coordinates])
+    add_if_not_yet_present(current_coordinates, east, facing.RIGHT if current_symbol == "-" or east not in coordinates_with_symbols else coordinates_with_symbols[current_coordinates])
+    add_if_not_yet_present(current_coordinates, south, facing.DOWN if current_symbol == "|" or south not in coordinates_with_symbols else coordinates_with_symbols[current_coordinates])
+    add_if_not_yet_present(current_coordinates, west, facing.LEFT if current_symbol == "-" or west not in coordinates_with_symbols else coordinates_with_symbols[current_coordinates])
 
 
 def update_starting_symbol(coordinates_with_symbols: dict[Coordinates:str], current_coordinates: Coordinates, north: Coordinates, east: Coordinates, south: Coordinates, west: Coordinates) -> str:
@@ -76,103 +77,194 @@ def update_starting_symbol(coordinates_with_symbols: dict[Coordinates:str], curr
             current_facing = facing.UP
         elif east_symbol in EAST_SYMBOLS:
             current_symbol = "L"
-            current_facing = facing.DOWN
+            current_facing = current_symbol
         elif west_symbol in WEST_SYMBOLS:
             current_symbol = "J"
-            current_facing = facing.DOWN
+            current_facing = current_symbol
     if east_symbol in EAST_SYMBOLS:
         if south_symbol in SOUTH_SYMBOLS:
             current_symbol = "F"
-            current_facing = facing.RIGHT
+            current_facing = current_symbol
         elif north_symbol in NORTH_SYMBOLS:
             current_symbol = "L"
-            current_facing = facing.DOWN
+            current_facing = current_symbol
         elif west_symbol in WEST_SYMBOLS:
             current_symbol = "-"
             current_facing = facing.LEFT
     if south_symbol in SOUTH_SYMBOLS:
         if east_symbol in EAST_SYMBOLS:
             current_symbol = "F"
-            current_facing = facing.LEFT
+            current_facing = current_symbol
         elif west_symbol in WEST_SYMBOLS:
             current_symbol = "7"
-            current_facing = facing.RIGHT
+            current_facing = current_symbol
 
     coordinates_with_symbols[current_coordinates] = current_symbol
     return current_facing
 
 
-def group_non_boarder_regions(non_boarder_coordinates: list[Coordinates], groups: list[list[Coordinates]]):
-    remaining = list(non_boarder_coordinates)
+def group_non_border_regions(non_border_coordinates: list[Coordinates], groups: list[list[Coordinates]]):
+    remaining = list(non_border_coordinates)
     group = [remaining[0]]
     groups.append(group)
     remaining.remove(group[0])
-    for element in remaining:
-        if coordinates.is_any_coordinate_adjacent(element, group):
-            group.append(element)
+    is_number_of_elements_different = True
+    while is_number_of_elements_different:
+        remaining_elements = len(remaining)
+        for element in remaining:
+            if coordinates.is_any_coordinate_adjacent(element, group, allow_diagonal=False):
+                group.append(element)
 
-    for member in group:
-        if member in remaining:
-            remaining.remove(member)
+        for member in group:
+            if member in remaining:
+                remaining.remove(member)
+
+        is_number_of_elements_different = remaining_elements != len(remaining)
 
     if len(remaining) != 0:
-        group_non_boarder_regions(remaining, groups)
+        group_non_border_regions(remaining, groups)
 
 
-def is_right_of_path_up(region_group: list[Coordinates], coordinates_with_facing: [dict[Coordinates:str]], connected_coordinates: list[Coordinates])->bool:
+def is_right_of_path_up(region_group: list[Coordinates], coordinates_with_facing: [dict[Coordinates:str]], connected_coordinates: list[Coordinates]) -> bool:
     is_right_of_path = None
     down_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, facing.UP)
     for position in down_symbols:
+        if is_right_of_path is not None:
+            break
         if connected_coordinates.index(position) < 2:
             continue
-        for border in region_group:
-            if Coordinates(position.x + 1, position.y) == border:
+        for region_element in region_group:
+            if Coordinates(position.x + 1, position.y) == region_element:
                 is_right_of_path = True
                 break
-            elif Coordinates(position.x - 1, position.y) == border:
+            elif Coordinates(position.x - 1, position.y) == region_element:
                 is_right_of_path = False
                 break
 
     if is_right_of_path is None:
         up_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, facing.UP)
         for position in up_symbols:
+            if is_right_of_path is not None:
+                break
             if connected_coordinates.index(position) < 2:
                 continue
-            for border in region_group:
-                if Coordinates(position.x - 1, position.y) == border:
+            for region_element in region_group:
+                if Coordinates(position.x - 1, position.y) == region_element:
                     is_right_of_path = True
                     break
-                elif Coordinates(position.x + 1, position.y) == border:
+                elif Coordinates(position.x + 1, position.y) == region_element:
                     is_right_of_path = False
                     break
 
     if is_right_of_path is None:
         right_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, facing.RIGHT)
         for position in right_symbols:
+            if is_right_of_path is not None:
+                break
             if connected_coordinates.index(position) < 2:
                 continue
-            for border in region_group:
-                if Coordinates(position.x, position.y - 1) == border:
+            for region_element in region_group:
+                if Coordinates(position.x, position.y + 1) == region_element:
                     is_right_of_path = True
                     break
-                elif Coordinates(position.x, position.y + 1) == border:
+                elif Coordinates(position.x, position.y - 1) == region_element:
                     is_right_of_path = False
                     break
 
     if is_right_of_path is None:
         left_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, facing.LEFT)
         for position in left_symbols:
+            if is_right_of_path is not None:
+                break
             if connected_coordinates.index(position) < 2:
                 continue
-            for border in region_group:
-                if Coordinates(position.x, position.y + 1) == border:
+            for region_element in region_group:
+                if Coordinates(position.x, position.y - 1) == region_element:
                     is_right_of_path = True
                     break
-                elif Coordinates(position.x, position.y - 1) == border:
+                elif Coordinates(position.x, position.y + 1) == region_element:
                     is_right_of_path = False
                     break
 
+    if is_right_of_path is None:
+        seven_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, "7")
+        for position in seven_symbols:
+            if is_right_of_path is not None:
+                break
+            index = connected_coordinates.index(position)
+            if index < 2:
+                continue
+            previous_symbol = coordinates_with_symbols[connected_coordinates[index - 1]]
+            for region_element in region_group:
+                if Coordinates(position.x + 1, position.y) == region_element or Coordinates(position.x, position.y - 1) == region_element:
+                    is_right_of_path = previous_symbol == "|" or previous_symbol == "L" or previous_symbol == "J"
+                    break
+
+    if is_right_of_path is None:
+        f_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, "F")
+        for position in f_symbols:
+            if is_right_of_path is not None:
+                break
+            index = connected_coordinates.index(position)
+            if index < 2:
+                continue
+            previous_symbol = coordinates_with_symbols[connected_coordinates[index - 1]]
+            for region_element in region_group:
+                if Coordinates(position.x - 1, position.y) == region_element or Coordinates(position.x, position.y - 1) == region_element:
+                    is_right_of_path = previous_symbol == "-" or previous_symbol == "J" or previous_symbol == "7"
+                    break
+
+    if is_right_of_path is None:
+        j_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, "J")
+        for position in j_symbols:
+            if is_right_of_path is not None:
+                break
+            index = connected_coordinates.index(position)
+            if index < 2:
+                continue
+            previous_symbol = coordinates_with_symbols[connected_coordinates[index - 1]]
+            for region_element in region_group:
+                if Coordinates(position.x + 1, position.y) == region_element or Coordinates(position.x, position.y + 1) == region_element:
+                    is_right_of_path = previous_symbol == "-" or previous_symbol == "F" or previous_symbol == "L"
+                    break
+
+    if is_right_of_path is None:
+        l_symbols = coordinates.find_symbols_in_grid(coordinates_with_facing, "L")
+        for position in l_symbols:
+            if is_right_of_path is not None:
+                break
+            index = connected_coordinates.index(position)
+            if index < 2:
+                continue
+            previous_symbol = coordinates_with_symbols[connected_coordinates[index - 1]]
+            for region_element in region_group:
+                if Coordinates(position.x - 1, position.y) == region_element or Coordinates(position.x, position.y + 1) == region_element:
+                    is_right_of_path = previous_symbol == "|" or previous_symbol == "F" or previous_symbol == "7"
+                    break
+
     return is_right_of_path
+
+
+def filter_for_neighbors(group: list[Coordinates], coordinates_with_facings: dict[Coordinates:str], add_neighbors_of_neighbors=True) -> dict[Coordinates:str]:
+    neighbors_to_group = {}
+    for position in group:
+        north = Coordinates(position.x, position.y - 1)
+        east = Coordinates(position.x + 1, position.y)
+        south = Coordinates(position.x, position.y + 1)
+        west = Coordinates(position.x - 1, position.y)
+
+        if north in coordinates_with_facings:
+            neighbors_to_group[north] = coordinates_with_facings[north]
+        if south in coordinates_with_facings:
+            neighbors_to_group[south] = coordinates_with_facings[south]
+        if east in coordinates_with_facings:
+            neighbors_to_group[east] = coordinates_with_facings[east]
+        if west in coordinates_with_facings:
+            neighbors_to_group[west] = coordinates_with_facings[west]
+
+    if add_neighbors_of_neighbors:
+        neighbors_to_group.update(filter_for_neighbors([neighbour for neighbour, symbol in neighbors_to_group.items()], coordinates_with_facings, add_neighbors_of_neighbors=False))
+    return neighbors_to_group
 
 
 lines = riddle_reader.read_file(riddle_reader.RIDDLE_FILE)
@@ -181,23 +273,39 @@ starting_position = coordinates.find_symbols_in_grid(coordinates_with_symbols, "
 
 print(f"starting position: {starting_position}")
 
+connected_coordinates = [starting_position]
+coordinates_with_facing = {}
+was_symbol_added = True
+while was_symbol_added:
+    previous_length = len(connected_coordinates)
+    get_symbol_neighbours(coordinates_with_symbols, connected_coordinates, coordinates_with_facing)
+    was_symbol_added = len(connected_coordinates) != previous_length
+
+print(f"The path length is {math.ceil(len(connected_coordinates) / 2)}")
+
+for position, symbol in coordinates_with_symbols.items():
+    if position not in connected_coordinates:
+        coordinates_with_symbols[position] = "."
+    else:
+        coordinates_with_symbols[position] = symbol
+
 all_empty_spaces = coordinates.find_symbols_in_grid(coordinates_with_symbols, ".")
 
 
-def is_not_in_contact_with_boarder(empty_coordinates: Coordinates, x_min=1, x_max=len(lines[0]), y_min=1, y_max=len(lines)) -> bool:
+def is_not_in_contact_with_border(empty_coordinates: Coordinates, x_min=1, x_max=len(lines[0]), y_min=1, y_max=len(lines)) -> bool:
     return empty_coordinates.x != x_min and empty_coordinates.x != x_max and empty_coordinates.y != y_min and empty_coordinates.y != y_max
 
 
-def is_in_contact_with_boarder(empty_coordinates: Coordinates) -> bool:
-    return not is_not_in_contact_with_boarder(empty_coordinates)
+def is_in_contact_with_border(empty_coordinates: Coordinates) -> bool:
+    return not is_not_in_contact_with_border(empty_coordinates)
 
 
 remaining_empty_coordinates = list(all_empty_spaces)
-border_region = list(filter(is_in_contact_with_boarder, all_empty_spaces))
+border_region = list(filter(is_in_contact_with_border, all_empty_spaces))
 
-nodes_added_to_boarder_region = True
+nodes_added_to_border_region = True
 previous_node_count = 0
-while nodes_added_to_boarder_region:
+while nodes_added_to_border_region:
 
     for test_coordinate in remaining_empty_coordinates:
         if test_coordinate not in border_region:
@@ -208,45 +316,52 @@ while nodes_added_to_boarder_region:
         if value in remaining_empty_coordinates:
             remaining_empty_coordinates.remove(value)
 
-    nodes_added_to_boarder_region = previous_node_count != len(border_region)
+    nodes_added_to_border_region = previous_node_count != len(border_region)
     previous_node_count = len(border_region)
 
-all_non_boarder_regions = [empty_coordinate for empty_coordinate in all_empty_spaces if empty_coordinate not in border_region]
-print(f"Empty spaces in boarder region: {border_region}, #: {len(border_region)}")
-print(f"Empty spaces not in boarder region: {all_non_boarder_regions}, #: {len(all_non_boarder_regions)}")
-
-groups = []
-group_non_boarder_regions(all_non_boarder_regions, groups)
-print(f"{len(groups)} groups of non boarder contacts could be identified")
-
-connected_coordinates = [starting_position]
-coordinates_with_facing = {}
-was_symbol_added = True
-while was_symbol_added:
-    previous_length = len(connected_coordinates)
-    get_symbol_neighbours(coordinates_with_symbols, connected_coordinates, coordinates_with_facing)
-    was_symbol_added = len(connected_coordinates) != previous_length
-
-print(connected_coordinates)
-
-print(f"The path length is {math.ceil(len(connected_coordinates) / 2)}")
-
-for row in range(1, len(lines[0]) + 2):
-    for column in range(1, len(lines) + 2):
-        current_coordinates = Coordinates(column, row)
+print("Pipe main loop after cleanup")
+for column in range(1, len(lines) + 1):
+    for row in range(1, len(lines[0]) + 1):
+        current_coordinates = Coordinates(row, column)
         if current_coordinates in connected_coordinates:
-            print(coordinates_with_facing[current_coordinates], end="")
-        else:
+            print(coordinates_with_symbols[current_coordinates], end="")
+        elif current_coordinates in border_region:
             print(".", end="")
-
+        else:
+            print("#", end="")
     print()
 
+print("\nFacings")
+for column in range(1, len(lines) + 1):
+    for row in range(1, len(lines[0]) + 1):
+        current_coordinates = Coordinates(row, column)
+        if current_coordinates in coordinates_with_facing:
+            print(coordinates_with_facing[current_coordinates], end="")
+        elif current_coordinates in border_region:
+            print(".", end="")
+        else:
+            print("#", end="")
+    print()
+
+all_non_border_regions = [empty_coordinate for empty_coordinate in all_empty_spaces if empty_coordinate not in border_region]
+print(f"Empty spaces in border region: {border_region}, #: {len(border_region)}")
+print(f"Empty spaces not in border region: {all_non_border_regions}, #: {len(all_non_border_regions)}")
+
+groups = []
+group_non_border_regions(all_non_border_regions, groups)
+print(f"{len(groups)} groups of non border contacts could be identified")
+
 is_border_right_of_path_up = is_right_of_path_up(border_region, coordinates_with_facing, connected_coordinates)
+print(f"Border is {'right' if is_border_right_of_path_up else 'left'} of path up")
 inside_groups = []
 for group in groups:
-    is_group_right_of_path_up = is_right_of_path_up(group, coordinates_with_facing, connected_coordinates)
+    print(f"Testing group {groups.index(group)}/{len(groups)} containing {len(group)} empty spots")
+    neighbors_to_group = filter_for_neighbors(group, coordinates_with_facing)
+    is_group_right_of_path_up = is_right_of_path_up(group, neighbors_to_group, connected_coordinates)
+    print(f"Group is {'right' if is_group_right_of_path_up else 'left'} of path up")
     if (is_group_right_of_path_up and not is_border_right_of_path_up) or (not is_group_right_of_path_up and is_border_right_of_path_up):
         inside_groups.append(group)
+        print(group)
 
-print(f"{len(inside_groups)} are inside the border")
-print(f"{reduce(lambda count, l: count + len(l), inside_groups, 0)} are inside the border")
+print(f"{len(inside_groups)} groups are inside the border")
+print(f"{reduce(lambda count, l: count + len(l), inside_groups, 0)} empty spaces are inside the border")
