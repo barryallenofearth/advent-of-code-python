7364034fd83e71ee459bf88fa32d968724c8f279
Michael <lordrevan1999@web.de>
2023-12-27 21:20:17 +0100

    AoC-stats namings

--- stats/read_stats.py
+++ stats/read_stats.py
@@ -37,7 +37,7 @@ for current_year in range(starting_year, latest_year + 1):
     year_frame = data[data["year"] == current_year]
     year_frame.sort_values("day")
 
-    data_bar_chart = go.Bar(x=year_frame["day"], y=year_frame["both_stars"], name="Data", marker_color="#e39032", marker_line_color='black', marker_line_width=2, opacity=1)
+    data_bar_chart = go.Bar(x=year_frame["day"], y=year_frame["both_stars"], name="Both stars", marker_color="#e39032", marker_line_color='black', marker_line_width=2, opacity=1)
 
     p0 = (year_frame["both_stars"].iloc[0], 4, 0)  # start with values near those we expect
     params, cv = scipy.optimize.curve_fit(exponential_function, year_frame["day"], year_frame["both_stars"], p0)
@@ -53,9 +53,9 @@ for current_year in range(starting_year, latest_year + 1):
 
     year_with_half_life.loc[len(year_with_half_life)] = [int(current_year), t_half]
 
-bar_chart = go.Bar(x=year_with_half_life["year"], y=year_with_half_life["half_life_time"], marker_color="#e39032", marker_line_color='black', marker_line_width=2, opacity=1)
+bar_chart = go.Bar(x=year_with_half_life["year"], y=year_with_half_life["half_life_time"], name="half life time", marker_color="#e39032", marker_line_color='black', marker_line_width=2, opacity=1)
 average_t_half = year_with_half_life["half_life_time"].mean()
-average_trace = go.Scatter(x=[starting_year - 0.5, latest_year + 0.5], y=[average_t_half, average_t_half], mode='lines', name='Fit', line_color="#281ed9", line_width=3)
+average_trace = go.Scatter(x=[starting_year - 0.5, latest_year + 0.5], y=[average_t_half, average_t_half], mode='lines', name='Average Value', line=dict(color='#281ed9', width=3, dash='dash'))
 half_life_image = go.Figure(data=[bar_chart, average_trace],
                             layout={"title": f"Half life time per year", "xaxis": {'title': {'text': "Year"}}, "yaxis": {'title': {'text': "Half life time (day)"}}})
 half_life_image.add_annotation(text=f"&#8709;={average_t_half:2.2f} day", align='left', showarrow=False, xref='paper', yref='paper', x=0.5, y=1.2, bordercolor='black', borderwidth=1)
