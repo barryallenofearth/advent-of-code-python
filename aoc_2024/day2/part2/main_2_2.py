76c2422c11c33fdcc4f4066d771d57f08c3b858d
Michael <lordrevan1999@web.de>
2024-12-02 16:56:14 +0100

    AoC-2024 day 2/2 complete (brute force)

--- aoc_2024/day2/part2/main_2_2.py
+++ aoc_2024/day2/part2/main_2_2.py
@@ -14,8 +14,7 @@ MAX_STEP_SIZE = 3
 number_of_safe_modes = 0
 
 
-def is_unsafe(numbers: list[int], original_numbers: list[int], removed_current_number=False, removed_previous_number=False, last_attempt=False) -> bool:
-    sequence_complete = False
+def is_unsafe(numbers: list[int]) -> bool:
     print(f"Process sequence {numbers}")
     start_number = numbers[0]
     second_number = numbers[1]
@@ -26,54 +25,36 @@ def is_unsafe(numbers: list[int], original_numbers: list[int], removed_current_n
         current_number = numbers[index]
         difference = math.fabs(current_number - previous_number)
         if (is_decreasing and current_number <= previous_number) or (not is_decreasing and current_number >= previous_number):
-            numbers = original_numbers[:]
 
-            print(f"Increment direction wrong {line.strip()}: previous: {previous_number}, current: {current_number} => " + (
-                "don't " if (removed_previous_number or removed_current_number) else "") + "tolerate")
-            if not removed_previous_number:
-                print(f"Remove previous number: {numbers[index - 1]}")
-                removed_previous_number = True
-                del numbers[index - 1]
-            elif not removed_current_number:
-                print(f"Remove current number: {numbers[index + 1]}")
-                removed_current_number = True
-                del numbers[index + 1]
-
-            break
+            print(f"Increment direction wrong {line.strip()}: previous: {previous_number}, current: {current_number}")
+            return True
         elif difference < MIN_STEP_SIZE or difference > MAX_STEP_SIZE:
-            numbers = original_numbers[:]
+            print(f"Step size wrong {line.strip()}: previous: {previous_number}, current: {current_number}")
+            return True
 
-            print(f"Step size wrong {line.strip()}: previous: {previous_number}, current: {current_number} => " + (
-                "don't " if (removed_previous_number or removed_current_number) else "") + "tolerate")
-            if not removed_previous_number:
-                removed_previous_number = True
-                print(f"Remove previous number: {numbers[index - 1]}")
-                del numbers[index - 1]
-            elif not removed_current_number:
-                removed_current_number = True
-                print(f"Remove current number: {numbers[index + 1]}")
-                del numbers[index + 1]
-            break
-        elif index == len(numbers) - 1:
-            sequence_complete = True
         previous_number = current_number
 
-    if not sequence_complete:
-        if not last_attempt:
-            return is_unsafe(numbers, original_numbers, removed_current_number, removed_previous_number, removed_previous_number and removed_current_number)
-        else:
-            return True
     return False
 
 
 with open(file_name) as file:
     for line in file:
         numbers = [int(value) for value in re.split(r"\s+", line.strip())]
-
-        if not is_unsafe(numbers, numbers, False, False):
+        if not is_unsafe(numbers):
             print(f"This sequence is safe {line.strip()}")
             number_of_safe_modes += 1
+            continue
         else:
-            print(f"This sequence is unsafe {line.strip()}")
+            is_safe = False
+            for index in range(len(numbers)):
+                current_sequence = numbers[:]
+                del current_sequence[index]
+                if not is_unsafe(current_sequence):
+                    print(f"This sequence is safe {line.strip()}")
+                    number_of_safe_modes += 1
+                    is_safe = True
+                    break
+            if not is_safe:
+                print(f"This sequence is unsafe {line.strip()}")
 
 print(f"The number of safe modes is {number_of_safe_modes}")
