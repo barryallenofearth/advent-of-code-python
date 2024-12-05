954b4b3da1fb9a7206b136517d011df85b9c0ab7
Michael Evelt <m.evelt@lvm.de>
2024-12-05 14:24:02 +0100

    aoc-init improve init year handling

--- init_year.py
+++ init_year.py
@@ -9,17 +9,17 @@ if not os.path.exists(f"aoc_{year}"):
     with open(f"aoc_{year}/__init__.py", "w") as file:
         print("create package init")
 
-    for day in range(1, 26):
+    for day in range(5, 26):
         os.mkdir(f"aoc_{year}/day{day}")
-        os.mkdir(f"aoc_{year}/day{day}/common")
-        with open(f"aoc_{year}/day{day}/common/.gitkeep", "w"):
-            print(f"create common folder .gitkeep file")
         with open(f"aoc_{year}/day{day}/riddle.txt", "w"):
             print(f"create package aoc_{year}/day{day}/riddle.txt")
+        with open(f"aoc_{year}/day{day}/test_riddle.txt", "w"):
+            print(f"create package aoc_{year}/day{day}/test_riddle.txt")
         for part in range(1, 3):
             part_path = f"aoc_{year}/day{day}/part{part}"
             os.makedirs(part_path)
 
             main_file = f"{part_path}/main_{day}_{part}.py"
-            with open(main_file, "w"):
+            with open(main_file, "w",encoding="utf-8") as main_file:
+                main_file.write('TEST_MODE = True\nif TEST_MODE:\n    file_name = "../test_riddle.txt"\nelse:\n    file_name = "../riddle.txt"\n')
                 print(f"create file {main_file}")
