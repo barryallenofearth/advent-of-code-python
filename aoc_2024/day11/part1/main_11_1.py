0e4bffc99db3c99c52e96906ba5365b7b8b406c8
Michael Evelt <m.evelt@lvm.de>
2024-12-13 12:42:49 +0100

    aoc-2024 day11/1 complete

--- aoc_2024/day11/part1/main_11_1.py
+++ aoc_2024/day11/part1/main_11_1.py
@@ -1,12 +1,29 @@
 def main():
-    TEST_MODE = True
+    TEST_MODE = False
     if TEST_MODE:
         file_name = "../test_riddle.txt"
     else:
         file_name = "../riddle.txt"
     with open(file_name, encoding="utf-8") as riddle_input:
         for line in riddle_input:
-            line = line.strip()
+            stones = line.strip().split(" ")
+
+        repetitions = 25
+        for round in range(repetitions):
+            print(f"Processing round {round}")
+            new_sequence = []
+            for stone in stones:
+                if int(stone) == 0:
+                    new_sequence.append("1")
+                elif len(stone) % 2 == 0:
+                    new_sequence.append(str(int(stone[:int(len(stone) / 2)])))
+                    new_sequence.append(str(int(stone[int(len(stone) / 2):])))
+                else:
+                    new_sequence.append(f"{int(stone) * 2024}")
+            stones = new_sequence
+            print(f"After {repetitions} blinks {len(stones)} stones.")
+
+        print(f"After {repetitions} blinks {len(stones)} stones.")
 
 
 if __name__ == "__main__":
